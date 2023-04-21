8063074de6c7415496455fbb5206f0477955d7af
nono-yuzu <nonnonpipipi@icloud.com>
2023-04-21 17:16:55 +0900

    Initial Commit

---
+++ StudyDiary_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudyDiary_SwiftUI
+//
+//  Created by 柳和花 on 2023/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyDiary_SwiftUI/StudyDiary_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyDiary_SwiftUIApp.swift
+//  StudyDiary_SwiftUI
+//
+//  Created by 柳和花 on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct StudyDiary_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
